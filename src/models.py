5cf2c151a413f2b092da7eaadf4d3b511b474b9f
akurz02 <161508355+akurz02@users.noreply.github.com>
2024-06-06 01:35:16 +0000

    Started building models

--- src/models.py
+++ src/models.py
@@ -2,18 +2,43 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
+class Person(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    name = db.Column(db.String(256), nullable=False)
+    homeworld = db.Column(db.String(256))
+    current_location = db.Column(db.Integer, db.ForeignKey('planet.id'))
+    
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<Person %r>' % self.name
 
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
+            "name": self.name,
+            "homeworld": self.homeworld,
+            "current_location": self.current_location,
             # do not serialize the password, its a security breach
         }
+    
+
+class Planet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(256), nullable=False)
+    biome = db.Column(db.String(256), nullable=False)
+    population = db.relationship('Person', backref='resident', lazy='dynamic')
+
+    
+
+    def __repr__(self):
+        return '<Planet %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "biome": self.biome,
+            # do not serialize the password, its a security breach
+        }
+    
+    
